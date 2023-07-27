e5a4208bb7a3349a1ecc36e05671a1fea3ff804a
Korjaw Korjaw <kornel.jaworski@gmail.com>
2023-07-28 01:38:50 +0200

    Add project files.

---
+++ Week2_Lesson7_2/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    private static void Main(string[] args)
+    {
+        /*
+            Napisz program w C#, który sprawdzi czy podana przez użytkownika liczba jest parzysta czy nieparzysta.
+            Dane testowe : 15
+            Rezultat w terminalu: 15 jest liczbą nieparzystą
+        */
+
+        int a = 15;
+
+        if (a % 2 == 0)
+        {
+            Console.WriteLine($"{a} jest parzysta");
+        }
+        else
+        {
+            Console.WriteLine($"{a} jest nieparzysta");
+        }
+    }
+
+}
